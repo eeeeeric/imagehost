2d123dd5db6d11a662684a8eb57d4438c1540b73
Eric <eeeeeric.github@gmail.com>
2014-01-16 02:14:45 -0600

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+        name='imagehost',
+        version='0.1',
+        description='upload to imagehosts from python',
+        author='Eric Zheng',
+        url='https://github.com/eeeeeric/imagehost',
+        packages=['imagehost']
+     )
